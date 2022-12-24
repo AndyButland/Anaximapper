64eb3793ee9f7275ea63da96a780e40e69e30199
Andy Butland <abutland73@gmail.com>
2022-12-24 12:02:43 +0000

    Added test site for V11.

---
+++ Anaximapper.TestSite.V11/Models/Attributes/MapFromContentPickerAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace Anaximapper.TestSite.Models.Attributes
+{
+    using System;
+    using System.Reflection;
+    using Umbraco.Cms.Core.Models.PublishedContent;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MapFromContentPickerAttribute : Attribute, IMapFromAttribute
+    {
+        public void SetPropertyValue<T>(object fromObject, PropertyInfo property, T model, MappingContext context)
+        {
+            var method = GetType().GetMethod("GetInstance", BindingFlags.NonPublic | BindingFlags.Instance);
+            var genericMethod = method.MakeGenericMethod(property.PropertyType);
+            var item = genericMethod.Invoke(this, new[] { fromObject, context.Mapper });
+            property.SetValue(model, item);
+        }
+
+        private T GetInstance<T>(object fromObject, IPublishedContentMapper mapper)
+            where T : class
+        {
+            T instance = default(T);
+            if (fromObject != null)
+            {
+                var content = fromObject as IPublishedContent;
+                if (content != null)
+                {
+                    instance = Activator.CreateInstance<T>();
+                    mapper.Map(content, instance);
+                }
+
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Anaximapper.TestSite.V11/Models/AutoMapping/NewsLandingPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Anaximapper.TestSite.Models
+{
+    using Anaximapper.Attributes;
+    using Microsoft.AspNetCore.Html;
+    using System;
+    using System.Collections.Generic;
+
+    public class NewsLandingPageViewModel
+    {
+        public NewsLandingPageViewModel()
+        {
+            NewsCategory = new Category();
+            TopStory = new NewsStory();
+            OtherStories = new List<NewsStory>();
+        }
+
+        [PropertyMapping(MapRecursively = true)]
+        public string Title { get; set; }
+
+        public string Heading { get; set; }
+
+        [PropertyMapping(LevelsAbove = 1)]
+        public Category NewsCategory { get; set; }
+
+        public NewsStory TopStory { get; set; }
+
+        public IEnumerable<NewsStory> OtherStories { get; set; }
+
+        public class Category
+        {
+            public string Title { get; set; }
+        }
+
+        public class NewsStory
+        {
+            public string Headline { get; set; }
+
+            public DateTime StoryDate { get; set; }
+
+            public HtmlString BodyText { get; set; }
+        }
+    }
+}
